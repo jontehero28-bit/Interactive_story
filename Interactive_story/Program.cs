e4eb7befb30c80195af7181df66ad12d2b3be53f
Yuriy Petrov <yuriy.petrov@elev.ga.ntig.se>
2022-09-02 12:26:47 +0200

    intial commit

---
+++ Interactive_story/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Console.WriteLine("Hello, who are you?");
+
+string name = Console.ReadLine();
+string ship = Console.ReadLine();
+Console.WriteLine($"Hello there, {name}");
+
+if (name == "Space Cowboy")
+{
+    Console.WriteLine("What is the name of your ship then?");
+}
+else
+{
+    Console.WriteLine("You are not the one I been waiting for. Die bitch *shoots you*");
+}
+
+if (ship == "Bebop")
+{
+Console.WriteLine("Okay, so it is you huh? I expected you to be bigger");
+}
+
+Console.ReadLine();
+
+
+
+
